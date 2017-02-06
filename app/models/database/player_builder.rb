fc99612b6e1651b7d06ffcfd3f1c944bf8479db8
Alan Covarrubias <aecovarru@gmail.com>
2017-02-06 01:17:51 -0800

    confirm logged in controllers

--- app/models/database/player_builder.rb
+++ app/models/database/player_builder.rb
@@ -5,8 +5,8 @@ module Database
       @season = Season.find_by_year(year)
       @season.teams.each do |team|
         doc = basketball_reference("/teams/#{team.abbr}/#{year}.html")
-        rows = doc.css("#roster td")
-        if rows
+        if doc
+          rows = doc.css("#roster td")
           rows.each_slice(8) do |row|
             create_player(row[0], @season, team)
           end
